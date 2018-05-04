c1305577646c5ce768271eeddc88ac0ab60cf91e
stilobique <aurelienvlt@free.fr>
2018-05-04 11:09:29 +0200

    Fix Pep8

--- BatchLightUE4/Views/Dial_About.py
+++ BatchLightUE4/Views/Dial_About.py
@@ -2,21 +2,23 @@ from PyQt5 import QtWidgets
 
 from BatchLightUE4.Views.Dial_About_convert import Ui_Help
 
+from BatchLightUE4.Controllers.Setup import Setup
 
-class Dial_View_About(QtWidgets.QDialog, Ui_Help):
+
+class DialViewAbout(QtWidgets.QDialog, Ui_Help):
     """This widget contains all help tabs ; information about number
     version, release and licence, and all shortcut inside the program."""
     def __init__(self, parent=None):
-        super(Dial_View_About, self).__init__(parent)
+        super(DialViewAbout, self).__init__(parent)
         self.setupUi(self)
 
         self.data = Setup()
 
         # Version Panel
         self.lineEdit.setText(self.data.number)
-        url_octi = self.label_url_octicons
+        url_icons = self.label_url_octicons
         url_website = self.label_url_website
-        url_octi.setText('''<a 
+        url_icons.setText('''<a 
         href='https://octicons.github.com/'>Github's Icons - Octicons</a>''')
         url_website.setText('''<a 
         href='https://github.com/stilobique/BatchBuildLightUE4/'>Github 
