260d291bf18913bf4d79c7c048436c36d9268692
stilobique <aurelienvlt@free.fr>
2018-05-04 11:08:10 +0200

    Fix Pep8

--- BatchLightUE4/Views/Dial_LogTools.py
+++ BatchLightUE4/Views/Dial_LogTools.py
@@ -3,10 +3,10 @@ from PyQt5 import QtWidgets
 from BatchLightUE4.Views.Dial_LogTools_convert import Ui_DialogLog
 
 
-class Dial_Log_Tools(QtWidgets.QDialog, Ui_DialogLog):
+class DialLogTools(QtWidgets.QDialog, Ui_DialogLog):
     """Log Panel"""
     def __init__(self, parent=None):
-        super(LogView, self).__init__(parent)
+        super(DialLogTools, self).__init__(parent)
         self.setupUi(self)
 
         print('Windows Log Show')
