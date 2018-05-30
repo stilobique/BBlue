c98464f13d41d9b5f85e5f689bfe464bd08ad73f
stilobique <aurelienvlt@free.fr>
2018-05-30 14:57:41 +0200

    Add a function to setup all label icon ; WIP

--- BatchLightUE4/Views/Dial_Rendering.py
+++ BatchLightUE4/Views/Dial_Rendering.py
@@ -1,3 +1,5 @@
+from unicodedata import name
+
 import psutil
 
 from PyQt5 import QtWidgets, QtCore, QtGui
@@ -25,36 +27,36 @@ class DialRendering(QtWidgets.QDialog, Ui_Rendering):
         self.levels_count = len(lvl_list)
 
         # Base UI
-        self.levels_works(self.levels)
+        self.levels_works()
         self.progress_bar_ui()
         self.buttons_box()
 
         # Launch all building light.
         self.building_light = ThreadRendering(lvl_list, csv, submit)
         self.building_light.start()
+        self.building_light.level_run.connect(self.level_icon)
         self.building_light.progress_value.connect(self.progress_bar_ui)
         self.building_light.button_ok.connect(self.buttons_box)
 
     # Ui Function -------------------------------------------------------------
     #   Generate the list with all levels rendering ---------------------------
-    def levels_works(self, lvl_list):
+    def levels_works(self):
         """
         Function to show all levels and what's is working.
-        :param lvl_list: list with all levels to build
+        :param run: str with the level on run
         :return:
         """
         group_parent = self.levels_group
         vertical_parent = self.layout_vertical
         vertical_parent.setAlignment(Qt.AlignJustify)
 
-        for item in lvl_list:
+        for item in self.levels:
             # Define horizontal layout
             h_layout = QtWidgets.QHBoxLayout()
             h_layout.setObjectName('h_layout')
             h_layout.setAlignment(Qt.AlignLeft)
 
             # Generate the logo used
-            lvl_list.index(item)
             icon = QtGui.QPixmap("Resources/Icons/s-empty.png")
 
             # Define all label
@@ -62,6 +64,7 @@ class DialRendering(QtWidgets.QDialog, Ui_Rendering):
             level_name.setText("Level rendering name :")
             level_logo = QtWidgets.QLabel(group_parent)
             level_logo.setText('')
+            level_logo.setObjectName(item)
             level_logo.setPixmap(icon)
             level_item = QtWidgets.QLabel(group_parent)
             level_item.setText(item)
@@ -70,6 +73,23 @@ class DialRendering(QtWidgets.QDialog, Ui_Rendering):
             h_layout.addWidget(level_logo, alignment=Qt.AlignCenter)
             h_layout.addWidget(level_item, alignment=Qt.AlignLeft)
             vertical_parent.addLayout(h_layout)
+
+    def level_icon(self, level):
+        """
+        Function to change the Icon used ; at the moment we have 3 states :
+        - Empty, the basic setup
+        - WIP, when one level has rendering
+        - Finish, to show all levels build.
+        :param level:
+        :return:
+        """
+        label = self.levels_group.findChildren(QtWidgets.QLabel)
+        icon = QtGui.QPixmap("Resources/Icons/s-zap.png")
+
+        for label_name in label:
+            item = label_name.objectName()
+            if item == level:
+                label_name.setPixmap(icon)
 
     #   Bottom Toolbars, option to launch the rendering and the log -----------
     def progress_bar_ui(self, value=0):
@@ -103,6 +123,7 @@ class DialRendering(QtWidgets.QDialog, Ui_Rendering):
 class ThreadRendering(QtCore.QThread):
     progress_value = pyqtSignal(int)
     button_ok = pyqtSignal(bool)
+    level_run = pyqtSignal(str)
 
     def __init__(self, level_rendering, csv, submit):
         """
@@ -120,18 +141,17 @@ class ThreadRendering(QtCore.QThread):
         self.lvl_list = level_rendering
         self.csv_data = csv
         self.submit = submit
-        self._running = True
 
     def __del__(self):
         self.wait()
 
     def run(self):
-        print('Hello, i am a thread')
         self.sleep(4)
 
         for level in self.lvl_list:
             swarm = build(level)
             count = self.lvl_list.index(level) + 1
+            self.level_run.emit(level)
             while swarm:
                 self.sleep(20)
                 if swarm.pid not in psutil.pids():
@@ -139,7 +159,5 @@ class ThreadRendering(QtCore.QThread):
                     break
 
             if count == len(self.lvl_list):
-                print('Finish rendering')
                 test = True
                 self.button_ok.emit(test)
-            print('End Looping Level > ', level)
