dc8f4c73963b420a2f3e1cca76809df1c71ec354
stilobique <aurelienvlt@free.fr>
2018-06-04 18:33:39 +0200

    Perforce operator, update with documentation and debug print

--- BatchLightUE4/Controllers/Perfoce.py
+++ BatchLightUE4/Controllers/Perfoce.py
@@ -11,32 +11,40 @@ from ..Models.Database import TableProgram
 # Connect to Perfoce to check all map (and lvl .uasset)
 # -----------------------------
 def p4_checkout(level_used):
+    """
+    Checkout all levels on a folder
+    :param level_used:
+    :return:
+    """
+    print('Generate a Changing List')
     data = TableProgram()
     data_levels = data.select_levels(name=level_used)
-    data_levels = data_levels[0][2]
+    path_level = data_levels[0][2]
+
+    project_path = data.select_paths()
+    project_path = dirname(project_path[0][2])
+    abs_path = normpath(project_path + '/Content/' + path_level)
 
     regex = r"^.*Perforce"
-    lvl_perforce = re.sub(regex, '', data_levels)
-    depot = dirname(lvl_perforce)
+    rel_path = re.sub(regex, '', abs_path)
+    depot = dirname(abs_path)
 
     p4 = perforce.connect()
     revisions = []
 
-    for filename in os.listdir(dirname(data_levels)):
-        filename = '/' + depot + "/" + filename
-        if '.uasset' in filename:
-            revisions.append(filename)
-
-        if '.umap' in filename:
-            revisions.append(filename)
+    for filename in os.listdir(depot):
+        rel_path = rel_path.replace('\\', '/')
+        file = r"/" + dirname(rel_path) + r"/" + filename
+        if '.uasset' or '.umap' in file:
+            revisions.append(file)
 
     level_used = level_used.replace('.umap', '')
     description = ("""[ProVolley][GFX][LightmapAuto] Automatic Build """
                    """Lightmap generate for the level """ + level_used)
     cl = p4.findChangelist(description)
+
     for i in range(len(revisions)):
-        file = revisions[i]
-        p4.ls(file)
+        p4.ls(revisions[i])
         cl.append(revisions[i])
 
     revisions.clear()
