72f3bf76092d290af52ef37cc07664cdb6579c63
stilobique <aurelienvlt@free.fr>
2018-06-14 16:22:50 +0200

    Add the debug mode setup (on the requirements file and on the main scripts)

--- main.py
+++ main.py
@@ -1,8 +1,15 @@
 import sys
+
 from PyQt5 import QtGui
 from PyQt5.QtWidgets import QApplication
 
 from BatchLightUE4.Views.MainWindows import MainWindows
+
+# Add option to the Debug mode
+if 'pydevd' in sys.modules:
+    import pydevd
+    pydevd.settrace('192.168.2.93', port=22,
+                    stdoutToServer=True, stderrToServer=True)
 
 app_info = 'B-BLUE4'
 
